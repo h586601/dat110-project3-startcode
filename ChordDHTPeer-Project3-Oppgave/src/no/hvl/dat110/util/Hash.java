7dac215943ee056e3d3b8352478bb84bc6804ae2
h586601 <586601@stud.hvl.no>
2021-03-22 14:52:09 +0100

    Task 1 & 2 completed. Tests for hashFunc and fileRep OK.

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Hash.java
@@ -11,56 +11,74 @@ import java.math.BigInteger;
 import java.net.UnknownHostException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 
-public class Hash { 
-	
-	private static BigInteger hashint; 
-	
-	public static BigInteger hashOf(String entity) {		
-		
-		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
+public class Hash {
+
+	private static BigInteger hashint;
+	private static MessageDigest md;
+
+	/**
+	 * Hash a given string with MD5 and returns the result as a BigInteger
+	 * 
+	 * @param entity
+	 * @return BigInteger
+	 */
+	public static BigInteger hashOf(String entity) {
+
 		// we use MD5 with 128 bits digest
-		
 		// compute the hash of the input 'entity'
-		
 		// convert the hash into hex format
-		
 		// convert the hex into BigInteger
-		
-		// return the BigInteger
-		
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		byte[] b = md.digest(entity.getBytes());
+
+		String hex = toHex(b);
+
+		hashint = new BigInteger(hex, 16);
+
 		return hashint;
 	}
-	
+
+	/**
+	 * Computes the address size of MD5
+	 * 
+	 * @return
+	 */
 	public static BigInteger addressSize() {
-		
-		// Task: compute the address size of MD5
-		
-		// get the digest length
-		
-		// compute the number of bits = digest length * 8
-		
+
+		int numberOfBits = bitSize();
+
 		// compute the address size = 2 ^ number of bits
-		
-		// return the address size
-		
-		return null;
+		BigInteger two = new BigInteger("2");
+		BigInteger addressSize = two.pow(numberOfBits);
+
+		return addressSize;
 	}
-	
+
+	/**
+	 * Finds the digest length and computes the number of bits
+	 * 
+	 * @return int bitSize
+	 */
 	public static int bitSize() {
-		
+
 		int digestlen = 0;
-		
-		// find the digest length
-		
-		return digestlen*8;
+
+		digestlen = md.getDigestLength();
+
+		return digestlen * 8;
 	}
-	
+
 	public static String toHex(byte[] digest) {
 		StringBuilder strbuilder = new StringBuilder();
-		for(byte b : digest) {
-			strbuilder.append(String.format("%02x", b&0xff));
+		for (byte b : digest) {
+			strbuilder.append(String.format("%02x", b & 0xff));
 		}
 		return strbuilder.toString();
 	}
